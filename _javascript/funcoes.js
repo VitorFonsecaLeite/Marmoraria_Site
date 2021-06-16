4d9e08b38bbc3c6e8faf5b93d6a38a4b9ca927d0
Vitor Fonseca <83980498+VitorFonsecaLeite@users.noreply.github.com>
2021-06-16 15:31:37 -0300

    adição ao portifolho
    Foram adicionadas novas fotos ao catalogo

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,7 +1,12 @@
 function mudaFoto(foto){
     document.getElementById("icone").src=foto;
 }
-
-function mostraFoto(foto){
-    document.getElementById("destaque").src=foto;
+function mostraFoto1(foto){
+    document.getElementById("destaque-01").src=foto;
+}
+function mostraFoto2(foto){
+    document.getElementById("destaque-02").src=foto;
+}
+function mostraFoto3(foto){
+    document.getElementById("destaque-03").src=foto;
 }
