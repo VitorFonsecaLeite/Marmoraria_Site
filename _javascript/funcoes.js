7df67561f65828a33fa5a6ee54ca06532f8fd61a
Vitor Fonseca <83980498+VitorFonsecaLeite@users.noreply.github.com>
2021-06-20 11:23:31 -0300

    Atualização de catalogo

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -10,3 +10,12 @@ function mostraFoto2(foto){
 function mostraFoto3(foto){
     document.getElementById("destaque-03").src=foto;
 }
+function mostraFoto4(foto){
+    document.getElementById("destaque-04").src=foto;
+}
+function mostraFoto5(foto){
+    document.getElementById("destaque-05").src=foto;
+}
+function mostraFoto6(foto){
+    document.getElementById("destaque-06").src=foto;
+}
