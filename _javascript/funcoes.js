1ca324ed1b50d127b8eb0ba8de413c2b69180671
Vitor Fonseca <83980498+VitorFonsecaLeite@users.noreply.github.com>
2021-07-06 15:00:31 -0300

    atualização de portefolho

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -19,3 +19,24 @@ function mostraFoto5(foto){
 function mostraFoto6(foto){
     document.getElementById("destaque-06").src=foto;
 }
+function mostraFoto7(foto){
+    document.getElementById("destaque-07").src=foto;
+}
+function mostraFoto8(foto){
+    document.getElementById("destaque-08").src=foto;
+}
+function mostraFoto9(foto){
+    document.getElementById("destaque-09").src=foto;
+}
+function mostraFoto10(foto){
+    document.getElementById("destaque-10").src=foto;
+}
+function mostraFoto11(foto){
+    document.getElementById("destaque-11").src=foto;
+}
+function mostraFoto12(foto){
+    document.getElementById("destaque-12").src=foto;
+}
+function mostraFoto13(foto){
+    document.getElementById("destaque-13").src=foto;
+}
